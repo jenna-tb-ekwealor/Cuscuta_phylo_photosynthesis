0b5f20e9d5dd550a7ddf56d1aba62d2dd0ae9a9c
Jenna T. B. Ekwealor <jtbe@berkeley.edu>
2025-02-07 22:01:04 -0800

    update

--- 02_pam/scripts/03_pam_along_stem.R
+++ 02_pam/scripts/03_pam_along_stem.R
@@ -14,52 +14,69 @@ current_path = rstudioapi::getActiveDocumentContext()$path
 setwd(dirname(current_path ))
 # print( getwd() )
 
-young<-"orange" 
-old<-"#924900" #Brown
+young <- "orange" 
+old <- "#924900" 
+
+Fo <- "#EC5E27"
+FvFm <- "#0FA379"
+PhiPSII <- "#A39F1C"
+PhiNPQ <- "#4B6637"
+
     
 # load df
 pam_df <- read.csv(file = "../data/Cuscuta_stem_ages_PAM.csv")
 
 
 #### Hierarchical mixed linear model (random effect = sample) ####
+
 ## Fv.Fm
 HLM <-lmer(Fv.Fm ~ log(Distance.from.AM) + (1|Biorep/Sample), data=pam_df)
 summary(HLM)
 
-## now repeat for PSII
+## PSII
 HLM2 <-lmer(PSII ~ log(Distance.from.AM) + (1|Biorep/Sample), data=pam_df)
 summary(HLM2)
 
-## now repeat for phiNPQ
+## phiNPQ
 HLM3 <-lmer(phiNPQ ~ log(Distance.from.AM) + (1|Biorep/Sample), data=pam_df)
 summary(HLM3)
 
-#### plot_Fv/Fm, PSII, and phiNPQ with lmer predictions ####
+## Fo
+HLM4 <-lmer(Fo ~ log(Distance.from.AM) + (1|Biorep/Sample), data=pam_df)
+summary(HLM3)
+
+#### plot_Fv/Fm, PSII, Fo, and phiNPQ with lmer predictions ####
+
 # select plotting data and convert to long
-
-
 pam <- ggplot() +
   ylim (0,0.7) +
-  annotate("rect", xmin = 1, xmax = 5, ymin = 0, ymax = 0.7,
-           alpha = .2,fill = young) +
-  annotate("rect", xmin = 5, xmax = 10, ymin = 0, ymax = 0.7,
-           alpha = .075,fill = young) +
-  annotate("text", x = 5.5, y = 0.08, label = "Young stem", color = young) +
+  # # add background rectangles
+  # # 1 - 5 cm used for fluorescence analyses, 1 - 10 for pigments
+  # annotate("rect", xmin = 1, xmax = 10, ymin = 0, ymax = 0.7, color = young, fill = young, alpha = 0) +
+  # annotate("rect", xmin = 19, xmax = 71, ymin = 0, ymax = 0.7, color = old, fill = old, alpha = 0) +
+  # 
+  # annotate("text", x = 5.5, y = 0.735, size = 3.5, label = "Young stem", color = young) +
+  # annotate("text", x = 45, y = 0.735, size = 3.5, label = "Old stem", color = old) +
+  # 
+  geom_point(data = pam_df, aes(x=Distance.from.AM, y=Fv.Fm), shape = 19, size = 2.5, stroke = 0, color = FvFm, alpha = 0.5) +
+  geom_line(data = pam_df, color = FvFm, aes(x=Distance.from.AM, y=predict(HLM), group=Sample), linewidth = 0.4) + 
   
-  annotate("rect", xmin = 19, xmax = 71, ymin = 0, ymax = 0.7,
-           alpha = .2,fill = old) +
-  annotate("text", x = 45, y = 0.08, label = "Old stem", color = old) +
+  geom_point(data = pam_df, aes(x=Distance.from.AM, y=PSII), shape = 19, size = 2.5, stroke = 0, color = PhiPSII, alpha = 0.5) +
+  geom_line(data = pam_df, color = PhiPSII, aes(x=Distance.from.AM, y=predict(HLM2), group=Sample), linewidth = 0.4) +
+
+  geom_point(data = pam_df, aes(x=Distance.from.AM, y=Fo), shape = 19, size = 2.5, stroke = 0, color = Fo, alpha = 0.5) +
+  # geom_line(data = pam_df, color = Fo, aes(x=Distance.from.AM, y=predict(HLM4), group=Sample), linewidth = 0.2) +
   
-  geom_point(data = pam_df, aes(x=Distance.from.AM, y=Fv.Fm), size=1.5, color = "darkgray") +
-  geom_line(data = pam_df, aes(x=Distance.from.AM, y=predict(HLM), group=Sample), linewidth = 0.5) + 
-  geom_point(data = pam_df, aes(x=Distance.from.AM, y=PSII), shape = 17, size=1.6, color = "darkgray") +
-  geom_line(data = pam_df, aes(x=Distance.from.AM, y=predict(HLM2), group=Sample), linetype = 'dashed', linewidth = 0.5) + 
-  theme_light() +
-  theme(axis.title.y = element_blank()) +
-  labs(x = "Distance from apical meristem (cm)") 
+  theme_minimal() +
+  theme(text = element_text(size = 14),
+        strip.text.x = element_text(angle = 0, face = "bold"),
+        strip.text.y.left = element_text(angle = 0, face = "bold"), 
+        legend.position = "none") +
+  labs(x = "Distance from apical meristem (cm)", 
+       y = "Fluorescence") 
 pam 
 
-pdf("../output/boxplots/pam_along_stem.pdf", width=6,height=4) 
+pdf("../output/boxplots/pam_along_stem.pdf", width=6,height=3.5) 
 pam
 dev.off()
 
