f150879ce4b75fe51c82a511ae0a882f95e4650b
Jenna T. B. Ekwealor <jtbe@berkeley.edu>
2025-02-12 16:29:59 -0800

    new figs

--- 02_pam/scripts/03_pam_along_stem.R
+++ 02_pam/scripts/03_pam_along_stem.R
@@ -50,14 +50,6 @@ summary(HLM3)
 # select plotting data and convert to long
 pam <- ggplot() +
   ylim (0,0.7) +
-  # # add background rectangles
-  # # 1 - 5 cm used for fluorescence analyses, 1 - 10 for pigments
-  # annotate("rect", xmin = 1, xmax = 10, ymin = 0, ymax = 0.7, color = young, fill = young, alpha = 0) +
-  # annotate("rect", xmin = 19, xmax = 71, ymin = 0, ymax = 0.7, color = old, fill = old, alpha = 0) +
-  # 
-  # annotate("text", x = 5.5, y = 0.735, size = 3.5, label = "Young stem", color = young) +
-  # annotate("text", x = 45, y = 0.735, size = 3.5, label = "Old stem", color = old) +
-  # 
   geom_point(data = pam_df, aes(x=Distance.from.AM, y=Fv.Fm), shape = 19, size = 2.5, stroke = 0, color = FvFm, alpha = 0.5) +
   geom_line(data = pam_df, color = FvFm, aes(x=Distance.from.AM, y=predict(HLM), group=Sample), linewidth = 0.4) + 
   
@@ -81,38 +73,45 @@ pam
 dev.off()
 
 
-# PSII <- ggplot() +
-#   ylim (0,0.7) +
-#   geom_point(data = pam_df, aes(x=Distance.from.AM, y=PSII), size=0.75, color = "gray") +
-#   geom_line(data = pam_df, aes(x=Distance.from.AM, y=predict(HLM2), group=Sample), size = 0.3) + 
-#   theme_light() +
-#   labs(x = "Distance from apical meristem (cm)",
-#        y = "phiPSII") 
-# PSII
-# 
-# pdf("../output/boxplots/phipsii_along_stem.pdf", width=4,height=4) 
-# PSII
-# dev.off()
+#### PAM v2 ####
+pamv2 <- ggplot() +
+  ylim (0,0.7) +
+  geom_point(data = pam_df, aes(x=Distance.from.AM, y=Fv.Fm), shape = 19, size = 2.5, stroke = 0, color = FvFm, alpha = 0.5) +
+  geom_line(data = pam_df, color = FvFm, aes(x=Distance.from.AM, y=predict(HLM), group=Sample), linewidth = 0.4) + 
+  
+  geom_point(data = pam_df, aes(x=Distance.from.AM, y=PSII), shape = 19, size = 2.5, stroke = 0, color = PhiPSII, alpha = 0.5) +
+  geom_line(data = pam_df, color = PhiPSII, aes(x=Distance.from.AM, y=predict(HLM2), group=Sample), linewidth = 0.4) +
+  
+  theme_minimal() +
+  theme(text = element_text(size = 14),
+        strip.text.x = element_text(angle = 0, face = "bold"),
+        strip.text.y.left = element_text(angle = 0, face = "bold"), 
+        legend.position = "none") +
+  labs(x = "Distance from apical meristem (cm)", 
+       y = "Fluorescence") 
+pamv2 
+
+
+pdf("../output/boxplots/pam_along_stem_v2.pdf", width=6,height=3.5) 
+pamv2
+dev.off()
+
+
+#### npq ####
 
 
 phiNPQ <- ggplot() +
   ylim (0,0.5) +
-  annotate("rect", xmin = 1, xmax = 5, ymin = 0, ymax = 0.5,
-           alpha = .2,fill = young) +
-  annotate("rect", xmin = 5, xmax = 10, ymin = 0, ymax = 0.5,
-           alpha = .075,fill = young) +
-  annotate("text", x = 5.5, y = 0.08, label = "Young stem", color = young) +
-  
-  annotate("rect", xmin = 19, xmax = 71, ymin = 0, ymax = 0.5,
-           alpha = .2,fill = old) +
-  annotate("text", x = 45, y = 0.08, label = "Old stem", color = old) +
-  geom_point(data = pam_df, aes(x=Distance.from.AM, y=phiNPQ), size=1.5) +
-  # geom_line(aes(y=predict(HLM3), group=Sample), size = 1, color="#ffbd00") + 
-  theme_light() +
-  theme(axis.title.y = element_blank())+
-  labs(x = "Distance from apical meristem (cm)")
+  geom_point(data = pam_df, aes(x=Distance.from.AM, y=phiNPQ), shape = 19, size = 2.5, color = PhiNPQ, alpha = 0.5, stroke = 0) +
+  theme_minimal() +
+  theme(text = element_text(size = 14),
+        strip.text.x = element_text(angle = 0, face = "bold"),
+        strip.text.y.left = element_text(angle = 0, face = "bold"), 
+        legend.position = "none") +
+  labs(x = "Distance from apical meristem (cm)", 
+       y = "PhiNPQ") 
 phiNPQ
 
-pdf("../output/boxplots/npq_along_stem.pdf", width=5,height=4)  
+pdf("../output/boxplots/npq_along_stem.pdf", width=6,height=3.5)  
 phiNPQ
 dev.off()
